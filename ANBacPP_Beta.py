dc14a4370553dab8160dd032f7b23616f986caae
naeemmrz <82546506+naeemmrz@users.noreply.github.com>
2022-02-21 22:52:12 +0300

    Update ANBacPP_Beta.py

--- ANBacPP_Beta.py
+++ ANBacPP_Beta.py
@@ -14,7 +14,7 @@ from padelpy import padeldescriptor
 image = Image.open('logo.png')
 st.image(image, use_column_width=True)
 st.write("""
-Developed by Naeem Abdul Ghafoor¹ & Ömür Baysal²
+Developed by Naeem Abdul Ghafoor¹ & Ömür Baysal¹
 \n
 
 User Input:
