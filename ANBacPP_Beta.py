49ddcdf6c0cc8cf2ee179a7b90c2006c43d2583c
naeemmrz <82546506+naeemmrz@users.noreply.github.com>
2022-02-21 22:56:07 +0300

    Update ANBacPP_Beta.py

--- ANBacPP_Beta.py
+++ ANBacPP_Beta.py
@@ -73,7 +73,7 @@ def download_link(object_to_download, download_filename, download_link_text):
 	if isinstance(object_to_download,pd.DataFrame):
 		object_to_download = object_to_download.to_csv(index=False)
 	b64 = base64.b64encode(object_to_download.encode()).decode()
-	return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
+	return f'<a href="data:text/plain;base64,{object_to_download}" download="{download_filename}">{download_link_text}</a>'
 
 
 # FRONTEND INPUTS
