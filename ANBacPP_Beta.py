76525604451d69cb3487af189f68c3efa9fa1a57
naeemmrz <82546506+naeemmrz@users.noreply.github.com>
2022-02-21 22:51:53 +0300

    Update ANBacPP_Beta.py

--- ANBacPP_Beta.py
+++ ANBacPP_Beta.py
@@ -70,14 +70,10 @@ def get_predictions(df, id_col):
   return results
 
 def download_link(object_to_download, download_filename, download_link_text):
-	
-    if isinstance(object_to_download,pd.DataFrame):
-        object_to_download = object_to_download.to_csv(index=False)
-
-    # some strings <-> bytes conversions necessary here
-    b64 = base64.b64encode(object_to_download.encode()).decode()
-
-    return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
+	if isinstance(object_to_download,pd.DataFrame):
+		object_to_download = object_to_download.to_csv(index=False)
+	b64 = base64.b64encode(object_to_download.encode()).decode()
+	return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
 
 
 # FRONTEND INPUTS
